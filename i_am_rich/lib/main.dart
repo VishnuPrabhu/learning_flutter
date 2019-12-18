b8e27b03b61d454f4b1190c4f5d23a5188679a29
Vishnu Prabhu Ramachandran <mailvishnuprabhu@gmail.com>
2019-12-18 07:43:12 +0530

    added app icon in flutter using appicon.co generator

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I am rich'),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
