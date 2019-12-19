b26f01671277f4a0315b9009a67d518069b348d4
vishnu <mailvishnuprabhu@gmail.com>
2019-12-19 16:38:26 +0530

    themedata tutorial - bmi calculator

---
+++ bmi-calculator-flutter-master/lib/constants.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: const Color(0xFF8D8EE98),
+);
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
